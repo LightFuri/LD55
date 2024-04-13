f445a900067eeb923841bc4bfbedda985946adc9
Stepan <94606541+StepanSage@users.noreply.github.com>
2024-04-13 15:01:20 +0300

    herf

--- InfernaPactum/Assets/Code/Logics/Services/ConstProvider.cs
+++ InfernaPactum/Assets/Code/Logics/Services/ConstProvider.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 
 public static class ConstProvider 
 {
-    public  const string PATH_BOOTSTRAPPER = "CanvasBootStrap.prefab";
+    public  const string PATH_BOOTSTRAPPER = "CanvasBootStrap";
     public const int LobbiLeavlID = 1;
 }
