ed9ea5480049bb6e7ec07b66be7d670a7ec1363f
Vadym Skliarov <vadym.s@code-care.pro>
2024-04-13 13:58:14 +0200

    Bootstrap fix

--- InfernaPactum/Assets/Code/Logics/Global/BootStraper.cs
+++ InfernaPactum/Assets/Code/Logics/Global/BootStraper.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class BootStraper : MonoBehaviour
 {
-    [SerializeField] private LoadLeaval _loadLeaval;
+    [SerializeField] private LoadLevell _loadLeaval;
 
     private void Awake()
     {
         var prefab = Resources.Load<GameObject>(ConstProvider.PATH_BOOTSTRAPPER);
         var canvas =  Instantiate(prefab);
-        _loadLeaval = canvas.GetComponent<LoadLeaval>();
+        _loadLeaval = canvas.GetComponent<LoadLevell>();
         _loadLeaval.Init(ConstProvider.LobbiLeavlID);
 
         DontDestroyOnLoad(this);
